10781d907f39e76636e118490d38a805f0d9ff83
Sakib963 <abdullah15-963@diu.edu.bd>
2023-06-07 17:00:57 +0600

    Created React App, Installed Tailwind, DaisyUI

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h3 className='text-5xl text-red-700 font-bold'>Elegant Edge Fashion School........</h3>
+    </>
+  )
+}
+
+export default App
