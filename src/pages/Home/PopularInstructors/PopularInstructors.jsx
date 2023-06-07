7f4ef64db52376508b6abd7ba5831f833e102213
Sakib963 <abdullah15-963@diu.edu.bd>
2023-06-08 04:03:26 +0600

    Added Popular Instructor Section

---
+++ src/pages/Home/PopularInstructors/PopularInstructors.jsx
@@ -0,0 +1,164 @@
+import { Link } from "react-router-dom";
+import { GrAddCircle } from "react-icons/gr";
+
+const PopularInstructors = () => {
+  return (
+    <div className="my-20 px-10">
+      <div className="text-center space-y-3">
+        <h3 className="text-4xl font-bold">World Class Instructors</h3>
+        <p className="font-semibold lg:w-2/4 mx-auto">
+          Unlock Your Potential with our Highly Sought-After Classes: Explore
+          the Most Popular Courses to Ignite Your Passion and Master Your Craft.
+        </p>
+      </div>
+      <div className="grid lg:grid-cols-3 gap-5 lg:w-3/4 mx-auto mt-10">
+        {/* Card 1 */}
+        <div className="space-y-3 border shadow-xl shadow-[#CDC7F8] rounded-lg p-10">
+          <img
+            className="mx-auto rounded-lg w-full"
+            src="https://i.ibb.co/J3x1GBC/instructor-1.png"
+            alt="Class Image"
+          />
+
+          <div className="space-y-2">
+            <h3 className="text-2xl font-semibold">Runway Ready</h3>
+            <div className="flex justify-between items-center">
+              <p>Total Classes: 10</p>
+            </div>
+
+            <div className="flex justify-center">
+              <Link className="w-full">
+                <button className="flex gap-2 justify-center items-center bg-[#CDC7F8] px-3 py-2 font-semibold rounded-md transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-[#A69BFB] duration-300 w-full">
+                  <GrAddCircle className="text-2xl" />
+                  Enroll Now
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+        {/* Card 2 */}
+        <div className="space-y-3 border shadow-xl shadow-[#CDC7F8] rounded-lg p-10">
+          <img
+            className="mx-auto rounded-lg w-full"
+            src="https://i.ibb.co/J3x1GBC/instructor-1.png"
+            alt="Class Image"
+          />
+
+          <div className="space-y-2">
+            <h3 className="text-2xl font-semibold">Runway Ready</h3>
+            <div className="flex justify-between items-center">
+              <p>Total Classes: 10</p>
+            </div>
+
+            <div className="flex justify-center">
+              <Link className="w-full">
+                <button className="flex gap-2 justify-center items-center bg-[#CDC7F8] px-3 py-2 font-semibold rounded-md transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-[#A69BFB] duration-300 w-full">
+                  <GrAddCircle className="text-2xl" />
+                  Enroll Now
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+        {/* Card 3 */}
+        <div className="space-y-3 border shadow-xl shadow-[#CDC7F8] rounded-lg p-10">
+          <img
+            className="mx-auto rounded-lg w-full"
+            src="https://i.ibb.co/J3x1GBC/instructor-1.png"
+            alt="Class Image"
+          />
+
+          <div className="space-y-2">
+            <h3 className="text-2xl font-semibold">Runway Ready</h3>
+            <div className="flex justify-between items-center">
+              <p>Total Classes: 10</p>
+            </div>
+
+            <div className="flex justify-center">
+              <Link className="w-full">
+                <button className="flex gap-2 justify-center items-center bg-[#CDC7F8] px-3 py-2 font-semibold rounded-md transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-[#A69BFB] duration-300 w-full">
+                  <GrAddCircle className="text-2xl" />
+                  Enroll Now
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+        {/* Card 4 */}
+        <div className="space-y-3 border shadow-xl shadow-[#CDC7F8] rounded-lg p-10">
+          <img
+            className="mx-auto rounded-lg w-full"
+            src="https://i.ibb.co/J3x1GBC/instructor-1.png"
+            alt="Class Image"
+          />
+
+          <div className="space-y-2">
+            <h3 className="text-2xl font-semibold">Runway Ready</h3>
+            <div className="flex justify-between items-center">
+              <p>Total Classes: 10</p>
+            </div>
+
+            <div className="flex justify-center">
+              <Link className="w-full">
+                <button className="flex gap-2 justify-center items-center bg-[#CDC7F8] px-3 py-2 font-semibold rounded-md transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-[#A69BFB] duration-300 w-full">
+                  <GrAddCircle className="text-2xl" />
+                  Enroll Now
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+        {/* Card 5 */}
+        <div className="space-y-3 border shadow-xl shadow-[#CDC7F8] rounded-lg p-10">
+          <img
+            className="mx-auto rounded-lg w-full"
+            src="https://i.ibb.co/J3x1GBC/instructor-1.png"
+            alt="Class Image"
+          />
+
+          <div className="space-y-2">
+            <h3 className="text-2xl font-semibold">Runway Ready</h3>
+            <div className="flex justify-between items-center">
+              <p>Total Classes: 10</p>
+            </div>
+
+            <div className="flex justify-center">
+              <Link className="w-full">
+                <button className="flex gap-2 justify-center items-center bg-[#CDC7F8] px-3 py-2 font-semibold rounded-md transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-[#A69BFB] duration-300 w-full">
+                  <GrAddCircle className="text-2xl" />
+                  Enroll Now
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+        {/* Card 6 */}
+        <div className="space-y-3 border shadow-xl shadow-[#CDC7F8] rounded-lg p-10">
+          <img
+            className="mx-auto rounded-lg w-full"
+            src="https://i.ibb.co/J3x1GBC/instructor-1.png"
+            alt="Class Image"
+          />
+
+          <div className="space-y-2">
+            <h3 className="text-2xl font-semibold">Runway Ready</h3>
+            <div className="flex justify-between items-center">
+              <p>Total Classes: 10</p>
+            </div>
+
+            <div className="flex justify-center">
+              <Link className="w-full">
+                <button className="flex gap-2 justify-center items-center bg-[#CDC7F8] px-3 py-2 font-semibold rounded-md transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-[#A69BFB] duration-300 w-full">
+                  <GrAddCircle className="text-2xl" />
+                  Enroll Now
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PopularInstructors;
