016bebf24f83f00d69e6357c5eaeeb4fc4acd05c
Sakib963 <abdullah15-963@diu.edu.bd>
2023-06-07 21:57:58 +0600

    Added Footer

--- src/components/CompanyLogo.jsx
+++ src/components/CompanyLogo.jsx
@@ -1,13 +1,35 @@
-import logo from '/elegant-logo.svg'
+import logo from "/elegant-logo.svg";
 
-
-const CompanyLogo = () => {
+const CompanyLogo = (isFooter) => {
+  if (isFooter) {
+    console.log('Footer')
     return (
-        <div className='flex h-4 lg:h-12 items-center justify-center gap-2'>
-            <img className='h-full' src={logo} alt="Elegant Edge Fashion School" />  
-            <h2 className='flex flex-col lg:text-3xl font-bold'>Elegant Edge</h2>          
+      <div className="flex justify-start">
+        <div className="flex h-4 lg:h-12 items-center justify-center gap-2">
+          <img
+            className="h-full"
+            src={logo}
+            alt="Elegant Edge Fashion School"
+          />
+          <h2 className="flex flex-col lg:text-3xl font-bold">Elegant Edge</h2>
         </div>
+      </div>
     );
+  }
+  else{
+    return (
+        <>
+            <div className="flex h-4 lg:h-12 items-center justify-center gap-2">
+              <img
+                className="h-full"
+                src={logo}
+                alt="Elegant Edge Fashion School"
+              />
+              <h2 className="flex flex-col lg:text-3xl font-bold">Elegant Edge</h2>
+            </div>
+          </>
+      )
+  }
 };
 
 export default CompanyLogo;
--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,10 +1,70 @@
+import CompanyLogo from "../../../components/CompanyLogo";
+import { BsFacebook, BsInstagram, BsYoutube } from "react-icons/bs";
 
 const Footer = () => {
-    return (
-        <div>
-            <h3>This is Footer</h3>            
+  return (
+    <div className="px-10 py-10 bg-[#CDC7F8]">
+      <div className="grid grid-cols-4 gap-4">
+        <CompanyLogo isFooter={true}></CompanyLogo>
+        <ul className="uppercase font-semibold">
+          <li className="hover:underline cursor-pointer">About</li>
+          <li className="hover:underline cursor-pointer">News</li>
+          <li className="hover:underline cursor-pointer">Classes</li>
+          <li className="hover:underline cursor-pointer">Career</li>
+          <li className="hover:underline cursor-pointer">Contact</li>
+        </ul>
+        <div className="flex">
+          <div className="flex gap-4">
+            <BsFacebook className="text-3xl cursor-pointer text-[#3b5998]" />
+            <BsInstagram className="text-3xl cursor-pointer text-[#E4405F]" />
+            <BsYoutube className="text-3xl cursor-pointer text-[#c4302b]" />
+          </div>
         </div>
-    );
+        <div className="space-y-2">
+          <h3 className="font-semibold">
+            Sign up and get discounted classes information.
+          </h3>
+          <div className="form-control">
+            <div className="input-group">
+              <input
+                type="text"
+                placeholder="Your Email"
+                className="input input-bordered"
+              />
+              <button className="btn btn-square">
+                <svg
+                  width="20"
+                  height="20"
+                  viewBox="0 0 20 20"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M2.5 16.6667V11.6667L9.16667 10L2.5 8.33334V3.33334L18.3333 10L2.5 16.6667Z"
+                    fill="black"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="divider"></div>
+     <div className="space-y-4">
+     <div className="flex justify-between font-semibold">
+        <h3>©2023 Elegant Edge Fashion School</h3>
+        <p className="flex gap-6 cursor-pointer">
+          Privacy Policy <span>Terms and Conditions</span>
+        </p>
+      </div>
+      <div>
+        <p>8502 Preston Rd, Inglewood, Maine 98380</p>
+        <p>(808) 555-0111</p>
+        <p>contact@elegantedge.com</p>
+      </div>
+     </div>
+    </div>
+  );
 };
 
 export default Footer;
